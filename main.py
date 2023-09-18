81dbefb4800fc5022b4a23a716118c0ec343c3c3
unknown <sepehr.90sam09@gmail.com>
2023-09-18 23:01:48 +0330

    portfolio

---
+++ main.py
@@ -0,0 +1,44 @@
+import csv
+import json
+from typing import Optional
+import pandas
+import numpy
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/<string:html_file>')
+def home(html_file: Optional[str] = None):
+    return render_template(html_file)
+
+
+def write_to_csv(data):
+    email = data['email']
+    password = data['password']
+    message = data['contact-message']
+    with open('csv.csv', 'a') as csv_file:
+        data = {
+            'email': [email],
+            'password': [password],
+            'message': [message]
+        }
+        new_data = pandas.DataFrame(data).to_csv(index=False, header=False)
+
+        csv_file.write(new_data)
+
+
+@app.route('/submit_form', methods=['post', 'get'])
+def submit_form():
+    if request.method == 'POST':
+        request_dict = request.form.to_dict()
+        write_to_csv(request_dict)
+        return 'Form submitted'
+
+    else:
+        return 'Somthing went wrong!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
